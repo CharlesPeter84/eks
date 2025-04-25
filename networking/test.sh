8bbb29b7dae67d29ed93a71ee1d2357d1dbf347e
Ubuntu <ubuntu@ip-10-0-2-94.ec2.internal>
2025-04-25 15:24:53 +0000

    Network Policy testing

--- networking/test.sh
+++ networking/test.sh
@@ -9,7 +9,7 @@ echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest1-dep1-app2 -n cptest1 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app1.cptest1
 
 echo "From: cptest2-dep2-app1"
-kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app1.cptest1
+kubectl exec -it deployment/cptest2-dep2-app1 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app1.cptest1
 
 echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app1.cptest1
@@ -25,7 +25,7 @@ echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest1-dep1-app2 -n cptest1 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app2.cptest1
 
 echo "From: cptest2-dep2-app1"
-kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app2.cptest1
+kubectl exec -it deployment/cptest2-dep2-app1 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app2.cptest1
 
 echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc1-app2.cptest1
@@ -42,7 +42,7 @@ echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest1-dep1-app2 -n cptest1 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app1.cptest2
 
 echo "From: cptest2-dep2-app1"
-kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app1.cptest2
+kubectl exec -it deployment/cptest2-dep2-app1 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app1.cptest2
 
 echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app1.cptest2
@@ -59,7 +59,7 @@ echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest1-dep1-app2 -n cptest1 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app2.cptest2
 
 echo "From: cptest2-dep2-app1"
-kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app2.cptest2
+kubectl exec -it deployment/cptest2-dep2-app1 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app2.cptest2
 
 echo "From: cptest1-dep1-app2"
 kubectl exec -it deployment/cptest2-dep2-app2 -n cptest2 -- curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}\n" svc2-app2.cptest2
